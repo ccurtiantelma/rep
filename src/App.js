5dec2f355024479f61973e09ff0fefa59f65262b
Claudio <110416418+ccurtiantelma@users.noreply.github.com>
2023-03-14 22:47:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import {} from "pagedjs/dist/paged.polyfill";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  font-size: 0.8rem;
+  grid-gap: 10px;
+`;
+
+const Col1 = styled.p`
+  grid-column: 1 / span 1;
+`;
+const Col2 = styled.p`
+  grid-column: 2 / span 1;
+  color: red;
+`;
+function App() {
+  return (
+    <Grid>
+      <Col1>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col1>
+      <Col2>
+        FURST 8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+      <Col1>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col1>
+      <Col2>
+        SECUND 8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+      <Col1>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col1>
+      <Col2>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+      <Col2>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+      <Col2>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+      <Col2>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+      <Col2>
+        8magna aliqua. Ut enim ad minim veniam, quis nostrud exeut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exe1Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur.aboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in cuui officia Lorem ipsum dolor
+        sit amet,
+      </Col2>
+    </Grid>
+  );
+}
+
+export default App;
